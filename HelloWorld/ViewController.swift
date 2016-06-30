c18bf1b3ca7f8c72b943aaa06a4a63c846275ffb
yasuhito <yasuhito@nagatayasuhito-no-macbook-air.local>
2016-07-01 04:23:06 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by yasuhito on 2016/07/01.
+//  Copyright © 2016年 yasuhito.nagata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
